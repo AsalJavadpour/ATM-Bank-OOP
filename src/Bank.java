a3c66db6789ee30edfc558989686a2414ec449ed
Asal Javadpour <asaljavadpour2882@gmail.com>
2024-03-14 12:40:38 +0330

    Bank app is created

---
+++ src/Bank.java
@@ -0,0 +1,10 @@
+
+public class Bank {
+	public static void main(String[] args) {
+		Account asal=new Account("Asal" , "IR2882");
+		Account athena=new Account("Athena" , "IR8320");
+		asal.showmenu();
+		athena.showmenu();
+	}
+	}
+
